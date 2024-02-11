b77a6a856d7d337cf269d7e836bada8eb08975db
carolahn <ahncarolina@gmail.com>
2024-02-11 15:51:47 -0300

    feat: login pages

---
+++ website/src/routes/ProtectedRoute.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Redirect, Route } from "react-router";
+import { useSelector } from "react-redux";
+
+const ProtectedRoute = (props) => {
+  const auth = useSelector((state) => state.auth);
+
+  if (auth.account) {
+    if (props.path === "/login") {
+      return <Redirect to={"/"} />;
+    }
+    return <Route {...props} />;
+  } else if (!auth.account) {
+    return <Redirect to={"/login"} />;
+  } else {
+    return <div>Not found</div>;
+  }
+};
+
+export default ProtectedRoute;
---
+++ website/src/store/slices/auth.jsx
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+
+const initialState = { token: null, refreshToken: null, account: null };
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setAuthTokens(state, action) {
+      state.refreshToken = action.payload.refreshToken;
+      state.token = action.payload.token;
+    },
+    setAccount(state, action) {
+      state.account = action.payload;
+    },
+    setLogout(state) {
+      state.account = null;
+      state.refreshToken = null;
+      state.token = null;
+    },
+  },
+});
+
+export default authSlice;
