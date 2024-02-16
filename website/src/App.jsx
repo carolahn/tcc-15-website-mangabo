f1833f31dc2a1ec49e642a44b1c9d51e6d3ffa4e
carolahn <ahncarolina@gmail.com>
2024-02-16 14:43:03 -0300

    feat: cache buster

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -5,20 +5,32 @@ import store, { persistor } from "./store";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import ProtectedRoute from "./routes/ProtectedRoute";
+import CacheBuster from 'react-cache-buster';
+import { version } from '../package.json';
+import Loading from './loading';
 
 export default function App() {
+  const isProduction = process.env.NODE_ENV === 'production';
   return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor} loading={null}>
-        <Router>
-          <div>
-            <Switch>
-              <Route exact path="/login" component={Login} />
-              <ProtectedRoute exact path="/" component={Profile} />
-            </Switch>
-          </div>
-        </Router>
-      </PersistGate>
-    </Provider>
+    <CacheBuster
+      currentVersion={version}
+      isEnabled={isProduction} //If false, the library is disabled.
+      isVerboseMode={false} //If true, the library writes verbose logs to console.
+      loadingComponent={<Loading />} //If not pass, nothing appears at the time of new version check.
+      metaFileDirectory={'.'} //If public assets are hosted somewhere other than root on your server.
+    >
+      <Provider store={store}>
+        <PersistGate persistor={persistor} loading={null}>
+          <Router>
+            <div>
+              <Switch>
+                <Route exact path="/login" component={Login} />
+                <ProtectedRoute exact path="/" component={Profile} />
+              </Switch>
+            </div>
+          </Router>
+        </PersistGate>
+      </Provider>
+    </CacheBuster>
   );
 }
